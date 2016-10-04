9b55146f15c38201c8a85ca5c355b511b9d4122b
Scott Endicott <sendicott1391@gmail.com>
2016-10-04 15:42:56 -0400

    Setting up the structure

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let browserify = require('gulp-browserify');
+
+gulp.task('default', ['html', 'css', 'js']);
+
+gulp.task('html', function () {
+    return gulp.src(['index.html', 'components/*.html'], { base: './' })
+        .pipe(gulp.dest('public/'));
+});
+
+gulp.task('css', function () {
+    return gulp.src('style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('app.js')
+        .pipe(browserify())
+        .pipe(gulp.dest('public/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('index.html', ['html']);
+    gulp.watch('components/*.html', ['html']);
+    gulp.watch('app.js', ['js']);
+    gulp.watch('*.scss', ['css']);
+});
